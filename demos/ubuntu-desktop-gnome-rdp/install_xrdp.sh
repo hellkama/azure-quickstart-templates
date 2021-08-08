bf9308feda64a01d42db4ef31d58f036b207a07c
hellkama <juhogii@gmail.com>
2021-08-08 14:42:57 +0300

    Update install_xrdp.sh

--- demos/ubuntu-desktop-gnome-rdp/install_xrdp.sh
+++ demos/ubuntu-desktop-gnome-rdp/install_xrdp.sh
@@ -16,18 +16,5 @@ sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.c
 #start remote desktop session
 sudo service xrdp restart
 
-#install Azure CLI
-apt-get update && sudo apt-get install -y azure-cli
-logger -t devvm "Azure-cli installed: $?"
-sudo apt-get -y update
-
-#install VSCode
-logger -t devvm "Installing VSCode: $?"
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install -y code
-logger -t devvm "VSCode Installed: $?"
 logger -t devvm "Success"
 exit 0
